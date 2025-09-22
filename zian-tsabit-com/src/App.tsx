8d7071006e04cef8853ef8543bfa3a7c1fa7e522
ZianTsabit <tsabitghazian@gmail.com>
2025-09-22 18:42:57 +0700

    remove footer

--- zian-tsabit-com/src/App.tsx
+++ zian-tsabit-com/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Books from "./pages/Books";
@@ -18,7 +17,6 @@ function App() {
         <Route path="/books" element={<Books />} />
         <Route path="/garage" element={<Garage />} />
       </Routes>
-      <Footer />
     </Router>
 
     </>
