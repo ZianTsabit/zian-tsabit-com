b8be9c2eece6b9f41625214ba68d05effcfc7123
ZianTsabit <tsabitghazian@gmail.com>
2025-09-28 00:30:16 +0700

    update about page

--- zian-tsabit-com/src/pages/About.tsx
+++ zian-tsabit-com/src/pages/About.tsx
@@ -5,7 +5,6 @@ import {
   Avatar,
   Divider
 } from "@mui/material";
-import Typewriter from "../components/Typewriter";
 
 
 function About() {
@@ -13,7 +12,7 @@ function About() {
   return (
     <Box
       sx={{
-        width: "85vh",
+        width: "75vh",
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
@@ -31,50 +30,6 @@ function About() {
           flexDirection: "row",
           alignItems: "center"
         }}  >
-
-        <Stack 
-          sx={{ 
-            display: "flex", 
-            flexDirection: "column", 
-            alignItems: "center",
-            bgcolor: "black",
-            padding: "12px",
-            borderRadius: "8px",
-            height: "100%",
-            width: "25%"
-          }}>
-          
-          <Avatar
-            alt="Zian Tsabit"
-            src="/pp-github.png"
-            sx={{ 
-                width: 120, 
-                height: 120,
-                marginRight: "8px",
-                marginLeft: "8px"
-            }}
-          />
-          
-          <Divider 
-            sx={{ 
-              bgcolor: "white", 
-              marginTop: "8px", 
-              marginBottom: "8px", 
-              width: "100%"
-            }} 
-          />
-          
-          <Typography
-            variant="body1"
-            component="div"
-            color="white"
-            sx={{ fontFamily: "'Ubuntu', sans-serif" }}
-          >
-            Ghazian Tsabit Alkamil
-          </Typography>
-        
-        </Stack>
-
         <Stack 
           sx={{ 
             display: "flex", 
@@ -83,30 +38,61 @@ function About() {
             padding: "12px",
             borderRadius: "8px",
             minHeight: "100%",
-            width: "75%",
+            width: "100%",
             textAlign: "justify",
             border: "1px solid white"
           }}>
-          <Typewriter text="Hello, I'm Ghazian" />
-          <Typewriter text="Software Engineer @ Cermati" />
+          <Typography 
+            variant="h5" 
+            component="div" 
+            sx={{ 
+              fontFamily: "'Ubuntu', sans-serif",
+              fontWeight: "700",
+              color: "white",
+              marginBottom: "12px",
+              textAlign: "left"
+            }}>
+              About Me
+          </Typography>
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{ 
+              width: "100%",
+              display: "flex", 
+              flexDirection: { xs: "column", sm: "row" },
+              alignItems: "center",
+              marginBottom: "12px"
+            }}  >
+            <Avatar
+              alt="Zian Tsabit"
+              src="/pp-github.png"
+              sx={{ 
+                  width: 120, 
+                  height: 120,
+                  marginRight: "8px",
+                  marginLeft: "8px"
+              }}
+            />
+          </Stack> 
           <Typography 
             variant="body1"
             component="div"
             sx={{
               fontFamily: "'Ubuntu', sans-serif",
-              fontSize: "24px",
+              fontSize: "18px",
               color: "#99AFD7",
               marginBottom: "12px",
               fontWeight: "700",
               lineHeight: "2",
-              letterSpacing: "2px",
+              letterSpacing: "1px",
               textAlign: "justify"
             }}>
-              Hi, I’m Ghazian Tsabit Alkamil, living in Jakarta, Indonesia.<br /><br />
-              I work as a Software Engineer on the Data Platform team at Cermati Fintech Group.<br /><br />
-              I studied Computer Science at the School of Electrical Engineering and Informatics, Bandung Institute of Technology.<br /><br />
-              Outside of work, I love spending time with books—especially Indonesian novels, with Eka Kurniawan as my favorite author—watching movies, and swimming, which I usually do about four times a week. <br /><br />
-              Music is also a big part of my life, and I’m a huge fan of The Beatles and Bob Dylan. <br /><br />
+              Hi, I’m Ghazian Tsabit Alkamil, living in Jakarta, Indonesia.
+              I work as a Software Engineer on the Data Platform team at Cermati Fintech Group.
+              I studied Computer Science at the School of Electrical Engineering and Informatics, Bandung Institute of Technology.
+              Outside of work, I love spending time with books—especially Indonesian novels, with Eka Kurniawan as my favorite author—watching movies, and swimming, which I usually do about four times a week.
+              Music is also a big part of my life, and I’m a huge fan of The Beatles and Bob Dylan.
               I enjoy learning new things, and recently I’ve started learning to play the guitar, inspired by the anime Bocchi the Rock!
           </Typography>
         </Stack>
